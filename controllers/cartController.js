09e81aed647e7e44ccf75c46ebf5ffb9448437b9
FCICoder <mahmoud.aadel1998@gmail.com>
2024-07-04 09:16:55 +0300

    'auth

---
+++ controllers/cartController.js
@@ -0,0 +1,55 @@
+import userModel from "../models/userModel.js";
+
+//! add items to user Cart
+const addToCart = async (req , res) => {
+    try{
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId] = 1;
+        }else {
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId , {cartData: cartData});
+        res.status(200).json({success: true, message: "Item added to cart successfully"});
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({success: false, message: "Error adding item to cart"});
+    }
+};
+
+//! remove items from user Cart 
+const removeFromCart = async (req , res) => {
+    try{
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(cartData[req.body.itemId]){
+            if(cartData[req.body.itemId] > 1){
+                cartData[req.body.itemId] -= 1;
+            }else {
+                delete cartData[req.body.itemId];
+            }
+        }
+
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData: cartData});
+        res.status(200).json({success: true, message: "Item removed from cart successfully"});
+    }catch(err){
+        res.status(500).json({success: false, message:  err.message});
+    }
+};
+
+//! fetch user Cart data
+const getCart = async (req , res) => {
+    try{
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        res.status(200).json({success: true, cartData});
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({success: false, message:  "Error while retrieving cart data"});
+    }
+}; 
+
+export { addToCart, removeFromCart, getCart };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js';
 //! App Configuration
 const app = express();
 const port = 4000;
@@ -18,9 +19,11 @@ app.get('/', (req , res) =>{
 })
 
 // ! api endpoint
-app.use("/api/food",foodRouter)
+app.use("/api/food",foodRouter);
 app.use('/images' , express.static('uploads'));
-app.use('/api/user' , userRouter)
+app.use('/api/user' , userRouter);
+app.use('/api/cart',cartRouter);
+
 //! Db Connection
 connectDB();
 
