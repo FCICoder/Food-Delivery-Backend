140c92ce1e946bb3f968a7eaf31b05557e481ab7
FCICoder <mahmoud.aadel1998@gmail.com>
2024-07-08 08:15:59 +0300

    'cors'

--- server.js
+++ server.js
@@ -11,14 +11,10 @@ const app = express();
 const port = 4000;
 
 //! Middlewares
-app.use(cors());
-app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
+app.use(cors({
+    origin:'*'
+}));
+
 //! Routes
 app.get('/', (req , res) =>{
     res.send('Api Working');
